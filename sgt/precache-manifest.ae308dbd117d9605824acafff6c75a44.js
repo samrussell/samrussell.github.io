4a53c3783a50a5b7fb22ffe2f22f67c3bfa6b585
Sam Russell <sam.h.russell@gmail.com>
2020-05-04 14:56:16 -0400

    Add serbian grammar trainer

---
+++ sgt/precache-manifest.ae308dbd117d9605824acafff6c75a44.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "1daa208c0af884938508a6515bf66311",
+    "url": "/sgt/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "3be1b6eea7aba91c0e21",
+    "url": "/sgt/static/js/app.7c08f65c.chunk.js"
+  },
+  {
+    "revision": "11cc97f7cbb2629caef299afde88abc6",
+    "url": "/sgt/static/js/app.7c08f65c.chunk.js.gz"
+  },
+  {
+    "revision": "bbdc48fa17057a765f99d38dc4c4e5e3",
+    "url": "/sgt/static/js/runtime~app.22e4b205.js.gz"
+  },
+  {
+    "revision": "8e7cf47d2b58f0d0ad83",
+    "url": "/sgt/static/js/runtime~app.22e4b205.js"
+  },
+  {
+    "revision": "55144606a6b4bd3e51b61fa90d74394f",
+    "url": "/sgt/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "f021b6037269cbd8a0e0099b4f1a5341",
+    "url": "/sgt/static/js/2.f212aeb3.chunk.js.gz"
+  },
+  {
+    "revision": "1ef05e93d98cb8bbacbb68fad46ca706",
+    "url": "/sgt/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/sgt/expo-service-worker.js"
+  },
+  {
+    "revision": "388c7b74d809b5e4b722",
+    "url": "/sgt/static/js/2.f212aeb3.chunk.js"
+  },
+  {
+    "revision": "16709f23227d220b53e235a942bf292c",
+    "url": "/sgt/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/sgt/serve.json"
+  },
+  {
+    "revision": "9c3308c96dd2a32c587ca8eef105ce18",
+    "url": "/sgt/index.html"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/sgt/favicon.ico"
+  },
+  {
+    "revision": "d70064ed8b9cdfd8dfb3b50a7bfa70f0",
+    "url": "/sgt/manifest.json"
+  },
+  {
+    "revision": "ba07666ef195efbe64003e00035008b2",
+    "url": "/sgt/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "82f4dc5c26b7967633fda42629f39d86",
+    "url": "/sgt/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "641afc06fa8aa7620d14945ccfde6b1f",
+    "url": "/sgt/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "11269499d9c3c5ccab3baa8d17634b35",
+    "url": "/sgt/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "7e3a51ecf5fbd46f3049423258216634",
+    "url": "/sgt/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "95134c8d88093ed687dbd90654d7b3f8",
+    "url": "/sgt/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "ac026fc04b656d0f6152a9e6cdd29a83",
+    "url": "/sgt/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "af0801f70e0d3be43842630bd67ab469",
+    "url": "/sgt/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "06c55e1a313994117d2cba90ada18f93",
+    "url": "/sgt/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/sgt/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/sgt/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/sgt/apple/icons/icon_180x180.png"
+  }
+];
