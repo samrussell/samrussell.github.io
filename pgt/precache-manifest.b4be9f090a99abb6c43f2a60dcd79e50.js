d0cbf5cc037bb4972f28ff3a42481f7dffde0bb9
Sam Russell <sam.h.russell@gmail.com>
2020-05-05 11:10:03 -0400

    Add Polish grammar trainer

---
+++ pgt/precache-manifest.b4be9f090a99abb6c43f2a60dcd79e50.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "1daa208c0af884938508a6515bf66311",
+    "url": "/pgt/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "e0054018c88ed1ffc02c",
+    "url": "/pgt/static/js/app.fecbaebd.chunk.js"
+  },
+  {
+    "revision": "2d4fea5d34ed7ed613d421a859952fd2",
+    "url": "/pgt/static/js/runtime~app.4d084dc4.js.gz"
+  },
+  {
+    "revision": "69c20ea458343636e999fa248d88b9dd",
+    "url": "/pgt/static/js/app.fecbaebd.chunk.js.gz"
+  },
+  {
+    "revision": "07b344cb0ce57c1c9fac",
+    "url": "/pgt/static/js/runtime~app.4d084dc4.js"
+  },
+  {
+    "revision": "55144606a6b4bd3e51b61fa90d74394f",
+    "url": "/pgt/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "ca265a253563aa0a54e7d4870060514a",
+    "url": "/pgt/static/js/2.ffe95599.chunk.js.gz"
+  },
+  {
+    "revision": "1ef05e93d98cb8bbacbb68fad46ca706",
+    "url": "/pgt/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/pgt/serve.json"
+  },
+  {
+    "revision": "6d768082db1ea7caafb2",
+    "url": "/pgt/static/js/2.ffe95599.chunk.js"
+  },
+  {
+    "revision": "11269499d9c3c5ccab3baa8d17634b35",
+    "url": "/pgt/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "06c55e1a313994117d2cba90ada18f93",
+    "url": "/pgt/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "ac026fc04b656d0f6152a9e6cdd29a83",
+    "url": "/pgt/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/pgt/expo-service-worker.js"
+  },
+  {
+    "revision": "95134c8d88093ed687dbd90654d7b3f8",
+    "url": "/pgt/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "82f4dc5c26b7967633fda42629f39d86",
+    "url": "/pgt/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "b3bdaf946d7d577c70e252b52236778e",
+    "url": "/pgt/manifest.json"
+  },
+  {
+    "revision": "16709f23227d220b53e235a942bf292c",
+    "url": "/pgt/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "af0801f70e0d3be43842630bd67ab469",
+    "url": "/pgt/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "7e3a51ecf5fbd46f3049423258216634",
+    "url": "/pgt/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "641afc06fa8aa7620d14945ccfde6b1f",
+    "url": "/pgt/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "ba07666ef195efbe64003e00035008b2",
+    "url": "/pgt/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "ce7a79ebde0556fdee452b8c41cd8c01",
+    "url": "/pgt/index.html"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/pgt/favicon.ico"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/pgt/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/pgt/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/pgt/apple/icons/icon_180x180.png"
+  }
+];
